b9c34694fdc40562e98a6ab35f923a9da34a64dd
Chi Ho Ho <harushochiho@gmail.com>
2025-04-30 15:08:19 -0400

    Added netlify.toml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {Button} from "primeng/button";
+import {AppModule} from "./app.module";
+import {ProjectsComponent} from "./child_components/projects/projects.component";
 
 @Component({
     selector   : 'app-root',
--- src/app/child_components/projects/projects.component.ts
+++ src/app/child_components/projects/projects.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CardModule} from "primeng/card";
 import {Divider} from "primeng/divider";
 import {ButtonModule} from "primeng/button";
 
 @Component({
-  selector   : 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrl   : './projects.component.css',
-  imports: [
-    CardModule,
-    Divider,
-    ButtonModule,
-  ]
+    selector   : 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrl   : './projects.component.css',
+    imports    : [
+        CardModule,
+        Divider,
+        ButtonModule
+    ]
 })
 export class ProjectsComponent {
 }
